058c1d305234ea7cabfe31642e45c494a90b1f93
Martin Mantler <mmantler@gmail.com>
2021-09-14 10:54:32 -0500

    updates

---
+++ Activities/Activity4.1/4.1.1-Evr-SelectingDOMElements.js
@@ -0,0 +1,16 @@
+// select all unordered list (ul) elements
+console.log(document.getElementsByTagName('ul'));
+console.log(document.querySelectorAll('ul'));
+
+// select all elements with the class "class-week"
+console.log(document.getElementsByClassName('class-week'));
+console.log(document.querySelectorAll('class-week'));
+
+// select all elements with the class "nav-bar"
+console.log(document.getElementsByClassName('.nav-bar'));
+console.log(document.querySelectorAll('nav-bark'));
+// select the element with the id "dog-picture" and save it to a variable
+let pic = document.getElementById('dog-picture');
+
+//
+console.log(document.querySelector('.class-week'));
---
+++ Activities/Activity4.1/4.1.2-Stu-NodeRelationships.js
@@ -0,0 +1,10 @@
+// select the root node
+let rn = document.getElementsByTagName('html');
+console.log(rn);
+// select the last child of the root node
+console.log(document.querySelector('html').lastElementChild);
+// select all the children of the body element
+
+// select the next sibling of the h2 node
+
+// select the parent element of the h1 node
